e983ee931879146ae34b1d87994d4cae800566db
gabzdyldaniel <50097720+gabzdyldaniel@users.noreply.github.com>
2019-05-06 10:57:22 +0200

    Update optimidoc.cs

--- optimidoc.cs
+++ optimidoc.cs
@@ -1,3 +1,4 @@
+using ConsoleApp7;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,83 +9,29 @@ namespace ConsoleApp28
 {
     class Program
     {
+        public static List<Customer> customerList = new List<Customer>() { };
+
         static void Main(string[] args)
         {
-            string reverse = "12345";
-            string hexToAscii = "31323341";
+            Customer customer = new Customer("12345", 1);
+            customerList.Add(customer);
+            Customer customer2 = new Customer("31323341", 2);
+            customerList.Add(customer2);
+            Customer customer3 = new Customer("123456", 3);
+            customerList.Add(customer3);
+            Customer customer4 = new Customer("12345678", 4);
+            customerList.Add(customer4);
+            Customer customer5 = new Customer("123456", 5);
+            customerList.Add(customer5);
+            Customer customer6 = new Customer("1E240", 6);
+            customerList.Add(customer6);
 
-            string substring = "12345678";
-            string swap = "123456";
-
-
-            Console.WriteLine(ReverseArray(reverse));
-            Console.WriteLine(ConvertHexToAscii(hexToAscii));
-
-            Console.WriteLine(SubstringCode(substring));
-            Console.WriteLine(SwapEachTwoChars(swap));
+            foreach (Customer cust in customerList)
+            {
+                Console.WriteLine(Customer.ValidateCustomer(cust));
+            }
 
             Console.ReadKey();
-        }
-
-        public static string ReverseArray(string codeToReverse)
-        {
-            char[] arrayToReverse = codeToReverse.ToCharArray();
-            Array.Reverse(arrayToReverse);
-
-            return new string(arrayToReverse);
-        }
-
-        public static string ConvertHexToAscii(string hexString)
-        {
-            try
-            {
-                string asciiCode = string.Empty;
-
-                for (int i = 0; i < hexString.Length; i += 2)
-                {
-                    string hs = string.Empty;
-
-                    hs = hexString.Substring(i, 2);
-                    uint decval = System.Convert.ToUInt32(hs, 16);
-                    char character = System.Convert.ToChar(decval);
-                    asciiCode += character;
-
-                }
-
-                return asciiCode;
-            }
-            catch (Exception ex) { Console.WriteLine(ex.Message); }
-
-            return string.Empty;
-        }
-
-        public decimal ConvertDecToHex(decimal dec)
-        {
-            decimal decNumber = 0;
-
-            return decNumber;
-        }
-
-        public static string SubstringCode(string code)
-        {
-            return code.Substring(0, 5);
-        }
-
-        public static string SwapEachTwoChars(string toSwap)
-        {
-            char[] charArrayToSwap = toSwap.ToCharArray();
-
-            if (charArrayToSwap.Length % 2 == 0)
-            {
-                for (int i = 0; i < charArrayToSwap.Length; i += 2)
-                {
-                    char pom = charArrayToSwap[i];
-                    charArrayToSwap[i] = charArrayToSwap[i + 1];
-                    charArrayToSwap[i + 1] = pom;
-                }
-            }
-
-            return new string(charArrayToSwap);
         }
     }
 }
